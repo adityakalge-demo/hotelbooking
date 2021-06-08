531ce4d11061034f06286ad2b5d40f012a26ec8d
adityakalge-demo <85448394+adityakalge-demo@users.noreply.github.com>
2021-06-08 22:28:13 +0530

    Added Test Cases
    Added Test Cases

--- hotelBooking/src/test/java/com/project/hotelBooking/HotelBookingApplicationTests.java
+++ hotelBooking/src/test/java/com/project/hotelBooking/HotelBookingApplicationTests.java
@@ -1,71 +1,89 @@
-package com.project.hotelBooking.Service;
+package com.project.hotelBooking;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 import com.project.hotelBooking.Repository.BookingRepository;
 import com.project.hotelBooking.Repository.RoomRepository;
 import com.project.hotelBooking.Repository.UserRepository;
+import com.project.hotelBooking.Service.BookingService;
 import com.project.hotelBooking.model.Booking;
 import com.project.hotelBooking.model.Room;
 import com.project.hotelBooking.model.User;
 
-@Service
-public class BookingService {
+@SpringBootTest
+class HotelBookingApplicationTests {
 	
-	@Autowired
-	private UserService userService;
-	@Autowired
-	private  RoomRepository hotelRepository;
-	@Autowired
+	@MockBean
+	private UserRepository userRepository;
+	
+	@MockBean
+	private RoomRepository roomRepository;
+	
+	@MockBean
 	private  BookingRepository bookingRepository;
 	
-	public String getBookingRoomStatus(long userId, long hotelId) {
-		User user =new User();
-		Room room = new Room();
-		Booking bookingObj = new Booking();
-		bookingObj.setUserId(userId);
-		bookingObj.setHotelId(hotelId);
-		Optional<User> userData = userService.findUserById(bookingObj.getUserId());
-		if(userData.isPresent()) {
-			user = userData.get();
-		}else {
-			return "User with provided ID not Present";
-		}
-		Optional<Room> hotelData = hotelRepository.findById(bookingObj.getHotelId());
-		if(hotelData.isPresent()) {
-			room = hotelData.get();
-			if(room.getRoomStatus().equals("Booked") || room.getRoomStatus().equals("Approval Pending")) {
-				return "Room already Booked or approval Pending";
-			}
-		}else {
-			return "Room with provided ID not Present";
-		}
-		return bookRoom(user, room, bookingObj);
-		
-	}
+	@Autowired
+	private BookingService bookingService;
 
-	private String bookRoom(User user, Room hotel, Booking bookingObj) {
-		if(user.getBonusPoints() >= hotel.getRoomRate()) {
-			bookingObj.setStatus("Booked");
-			user.setPreviousBonusPoints(user.getBonusPoints());
-			user.setBonusPoints(user.getBonusPoints() - hotel.getRoomRate());
-			userService.saveUser(user);
-			bookingObj.setUserName(user.getUserName());
-			hotel.setRoomStatus("Booked");
-			Booking booking = bookingRepository.save(bookingObj);
-			return "Room Booked with booking ID: " + booking.getBookingId() + " Booking Status:" + booking.getStatus();
-		}else {
-			bookingObj.setStatus("Approval Pending");
-			user.setPreviousBonusPoints(user.getBonusPoints());
-			user.setBonusPoints(user.getBonusPoints() - hotel.getRoomRate());
-			userService.saveUser(user);
-			bookingObj.setUserName(user.getUserName());
-			hotel.setRoomStatus("Approval Pending");
-			Booking booking = bookingRepository.save(bookingObj);
-			return "Room Booked with booking ID: " + booking.getBookingId() + " Booking Status:" + booking.getStatus();
-		}
+	@Test
+	void testBookingRoomWhenRoomNotPresent() {
+		Optional<User> optional= Optional.of(new User("Test",1000,200));
+		Optional<Room> room= Optional.empty();
+		when(userRepository.findById((long) 1)).thenReturn(optional);
+		when(roomRepository.findById((long) 1)).thenReturn(room);
+		
+		assertEquals("Room with provided ID not Present", bookingService.getBookingRoomStatus(1, 1));
+		
+	   
+	}
+	
+	@Test
+	void testBookingRoomWhenRoomAlreadyBooked() {
+		Optional<User> optional= Optional.of(new User("Test",1000,200));
+		Optional<Room> room= Optional.of(new Room("AC", 100, "Booked"));
+		when(userRepository.findById((long) 2)).thenReturn(optional);
+		when(roomRepository.findById((long) 2)).thenReturn(room);
+		
+		assertEquals("Room already Booked or approval Pending", bookingService.getBookingRoomStatus(2, 2));
+		
+	   
+	}
+	
+	@Test
+	void testBookingRoomWhenUserNotPresent() {
+		Optional<User> optional= Optional.empty();
+		Optional<Room> room= Optional.of(new Room("AC", 100, "Booked"));
+		when(userRepository.findById((long) 3)).thenReturn(optional);
+		when(roomRepository.findById((long) 3)).thenReturn(room);
+		
+		assertEquals("User with provided ID not Present", bookingService.getBookingRoomStatus(3, 3));
+		
+	   
+	}
+	
+	@Test
+	void testBookingWhenBonusPointsareMore() {
+		Optional<User> optional= Optional.of(new User("Test",1000,200));
+		Optional<Room> room= Optional.of(new Room("AC", 100, "UnBooked"));
+		Booking booking = new Booking();
+		booking.setBookingId(1);
+		booking.setStatus("Booked");
+		when(userRepository.findById((long) 4)).thenReturn(optional);
+		when(roomRepository.findById((long) 4)).thenReturn(room);
+		when(bookingRepository.save(Mockito.any())).thenReturn(booking);
+		String actual="Room Booked with booking ID: " + booking.getBookingId() + " Booking Status:" + booking.getStatus();
+		assertEquals(actual, bookingService.getBookingRoomStatus(4, 4));
+		
 	}
 }
